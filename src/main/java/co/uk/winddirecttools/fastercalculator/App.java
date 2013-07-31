89e5a408a504584bced592e10daf945c6693c468
jonathan.huck <jonathan.huck@wp-lt-000725.windgroup.windprospect.com>
2013-07-31 09:53:14 +0100

    Command line version ready for testing

--- src/main/java/co/uk/winddirecttools/fastercalculator/App.java
+++ src/main/java/co/uk/winddirecttools/fastercalculator/App.java
@@ -2,11 +2,8 @@ package co.uk.winddirecttools.fastercalculator;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.InvalidGridGeometryException;
 import org.geotools.coverage.grid.io.AbstractGridCoverage2DReader;
@@ -33,80 +30,100 @@ public class App {
     public static void main(String[] args) {
         try {
 
-            //get input directory path
-            String inPath;
-            JFileChooser inChooser = new JFileChooser();
-            inChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int inReturnVal = inChooser.showOpenDialog(inChooser);
-            if (inReturnVal == JFileChooser.APPROVE_OPTION) {
-                inPath = inChooser.getSelectedFile().getAbsolutePath();
-            } else {
-                return;
-            }
+            if (args.length == 3) {
 
-            //get list of files
-            String fileName;
-            File folder = new File(inPath);
-            File[] listOfFiles = folder.listFiles();
-            ArrayList<File> filesToProcess = new ArrayList<File>();
-            for (int i = 0; i < listOfFiles.length; i++) {
-                fileName = listOfFiles[i].getName();
-                if (listOfFiles[i].isFile()) {
-                    if (fileName.endsWith(".asc") || fileName.endsWith(".ASC")
-                            || fileName.endsWith(".tif") || fileName.endsWith(".TIF")) {
-                        filesToProcess.add(listOfFiles[i]);
+                //get radius (verify number)
+                int radius = 0;
+                try {
+                    radius = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Argument 1 for radius" + " must be an integer");
+                    System.exit(1);
+                }
+
+                //get in directory and verify
+                File inDirectory = new File(args[1]);
+                if (inDirectory.isDirectory()) {
+                    System.err.println("Argument 2 must be a valid directory");
+                    System.exit(1);
+                }
+
+                //get list of files
+                String fileName;
+                File[] listOfFiles = inDirectory.listFiles();
+                ArrayList<File> filesToProcess = new ArrayList<File>();
+                for (int i = 0; i < listOfFiles.length; i++) {
+                    fileName = listOfFiles[i].getName();
+                    if (listOfFiles[i].isFile()) {
+                        if (fileName.endsWith(".asc") || fileName.endsWith(".ASC")
+                                || fileName.endsWith(".tif") || fileName.endsWith(".TIF")) {
+                            filesToProcess.add(listOfFiles[i]);
+                        }
                     }
                 }
-            }
 
-            //get output file path
-            String outPath;
-            JFileChooser outChooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("GeoTiff Files", "tif");
-            outChooser.setFileFilter(filter);
-            int returnVal = outChooser.showSaveDialog(null);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                outPath = outChooser.getSelectedFile().getAbsolutePath();
+                //get out file and verify
+                File outFile = new File(args[2]);
+                if (Files.notExists(outFile.getParentFile().toPath())) {
+                    System.err.println("Argument 2 must be a *.tif file in a valid directory");
+                    System.exit(1);
+                }
+
+                //set default crs
+                CoordinateReferenceSystem crs = CRS.decode("EPSG:27700", true);
+                final Hints hint = new Hints();
+                hint.put(Hints.DEFAULT_COORDINATE_REFERENCE_SYSTEM, crs);
+
+                //read all input layers into an ArrayList of GridCoverage2D's
+                ArrayList<GridCoverage2D> coverages = new ArrayList<GridCoverage2D>();
+                for (File file : filesToProcess) {
+                    AbstractGridFormat format = GridFormatFinder.findFormat(file);
+                    AbstractGridCoverage2DReader reader = format.getReader(file, hint);
+                    coverages.add((GridCoverage2D) reader.read(null));
+                }
+
+                //build raster calculator
+                RasterCalculator rc = new RasterCalculator();
+                GridCoverage2D gc = rc.process(coverages, radius, RasterCalculator.ADD);
+
+                //write result
+                writeGeoTiffFile(gc, outFile.getAbsolutePath());
+                System.out.println("Done!");
+                
             } else {
-                return;
-            }//*/
-
-            //set default crs
-            CoordinateReferenceSystem crs = CRS.decode("EPSG:27700", true);
-            final Hints hint = new Hints();
-            hint.put(Hints.DEFAULT_COORDINATE_REFERENCE_SYSTEM, crs);
-
-            //read all input laters into ArrayList
-            ArrayList<GridCoverage2D> coverages = new ArrayList<GridCoverage2D>();
-            for (File file : filesToProcess) {
-
-                //read the next file
-                AbstractGridFormat format = GridFormatFinder.findFormat(file);
-                AbstractGridCoverage2DReader reader = format.getReader(file, hint);
-                coverages.add((GridCoverage2D) reader.read(null));
+                System.out.println("Sorry, I'm a 3 arguments kind of girl");
+                System.out.println("Maybe you need to put some speech-marks around your file-paths...?");
+                System.out.println("");
+                System.out.println("java -jar fastercalculator [radius] [input directory] [output file *.tif]");
+                System.exit(0);
             }
-
-            //build raster calculator
-            RasterCalculator rc = new RasterCalculator();
-            GridCoverage2D gc = rc.process(coverages, 20000, RasterCalculator.ADD);
-
-            //write result
-            writeGeoTiffFile(gc, outPath);  //*/
-            System.out.println("Done!");
 
         } catch (InvalidGridGeometryException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
         } catch (TransformException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalArgumentException ex) { //*/
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
+        } catch (IllegalArgumentException ex) {
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
         } catch (IOException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
         } catch (NoSuchAuthorityCodeException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
         } catch (FactoryException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("How embarassing...");
+            System.err.println(ex.toString());
+            System.exit(1);
         }
+
     }
 
     /**
@@ -131,3 +148,23 @@ public class App {
         }
     }
 }
+/*/get input directory path
+ String inPath;
+ JFileChooser inChooser = new JFileChooser();
+ inChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+ int inReturnVal = inChooser.showOpenDialog(inChooser);
+ if (inReturnVal == JFileChooser.APPROVE_OPTION) {
+ inPath = inChooser.getSelectedFile().getAbsolutePath();
+ } else {
+ return;
+ }
+ //get output file path
+ JFileChooser outChooser = new JFileChooser();
+ FileNameExtensionFilter filter = new FileNameExtensionFilter("GeoTiff Files", "tif");
+ outChooser.setFileFilter(filter);
+ int returnVal = outChooser.showSaveDialog(null);
+ if (returnVal == JFileChooser.APPROVE_OPTION) {
+ outPath = outChooser.getSelectedFile().getAbsolutePath();
+ } else {
+ return;
+ }   //*/
